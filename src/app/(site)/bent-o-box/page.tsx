ddb08e4fcfc21579e239f2511150a14869444371
Shreeya Singh <shreeya.react@gmail.com>
2026-08-25 15:11:18 +0530

    new

--- src/app/(site)/bent-o-box/page.tsx
+++ src/app/(site)/bent-o-box/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import BentoBoxComingSoon from "@/components/Home/BentoBoxComingSoon";
 
 export const metadata: Metadata = {
   title: "Bent-o-Box",
@@ -6,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function BentOBoxPage() {
-  return < />;
+  return <BentoBoxComingSoon />;
 }
